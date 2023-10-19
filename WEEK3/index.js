c7258722e45c4df376e5c9a5769313e3e1d07341
Devansh-Taneja <145650332+Devansh-Taneja@users.noreply.github.com>
2023-10-19 17:02:53 +0530

    changes

--- WEEK3/index.js
+++ WEEK3/index.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     var form = document.getElementById('MYFORM');
-    var button = document.getElementById('Openform');
-    var sp = document.getElementsByClassName('close')[0];
-    
-    button.onclick = function() {
+    var openFormButton = document.getElementById('Openform');
+    var closeFormButton = document.getElementById('closeForm');
+
+    openFormButton.addEventListener('click', function() {
         form.style.display = 'block';
-    }
-    
-    sp.onclick = function() {
+    });
+
+    closeFormButton.addEventListener('click', function() {
         form.style.display = 'none';
-    }
-    
-    window.onclick = function(event) {
-        if (event.target == form) {
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target === form) {
             form.style.display = 'none';
         }
-    }
-    
+    });
+
     document.getElementById('registration-Form').addEventListener('submit', function(event) {
         event.preventDefault();
         validateUsername();
@@ -29,61 +29,69 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('email').addEventListener('blur', validateEmail);
     document.getElementById('password').addEventListener('blur', validatePassword);
     document.getElementById('confirmPassword').addEventListener('blur', validateConfirmPassword);
-    
+
     function validateUsername() {
         var username = document.getElementById('username').value.trim();
         var errorElement = document.getElementById('usernameError');
+        var usernameInput = document.getElementById('username');
+
         if (username === '') {
             errorElement.textContent = 'Username cannot be empty';
-            document.getElementById('username').classList.add('error');
+            usernameInput.classList.add('error');
         } else {
             errorElement.textContent = '';
-            document.getElementById('username').classList.remove('error');
-            document.getElementById('username').classList.add('success');
+            usernameInput.classList.remove('error');
+            usernameInput.classList.add('success');
         }
     }
-    
+
     function validateEmail() {
         var email = document.getElementById('email').value.trim();
         var errorElement = document.getElementById('emailError');
+        var emailInput = document.getElementById('email');
         var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
         if (email === '') {
             errorElement.textContent = 'Email cannot be empty';
-            document.getElementById('email').classList.add('error');
+            emailInput.classList.add('error');
         } else if (!emailRegex.test(email)) {
             errorElement.textContent = 'Invalid email format';
-            document.getElementById('email').classList.add('error');
+            emailInput.classList.add('error');
         } else {
             errorElement.textContent = '';
-            document.getElementById('email').classList.remove('error');
-            document.getElementById('email').classList.add('success');
+            emailInput.classList.remove('error');
+            emailInput.classList.add('success');
         }
     }
-    
+
     function validatePassword() {
         var password = document.getElementById('password').value;
         var errorElement = document.getElementById('passwordError');
+        var passwordInput = document.getElementById('password');
+
         if (password.length < 8) {
             errorElement.textContent = 'Password must be at least 8 characters long';
-            document.getElementById('password').classList.add('error');
+            passwordInput.classList.add('error');
         } else {
             errorElement.textContent = '';
-            document.getElementById('password').classList.remove('error');
-            document.getElementById('password').classList.add('success');
+            passwordInput.classList.remove('error');
+            passwordInput.classList.add('success');
         }
     }
-    
+
     function validateConfirmPassword() {
         var password = document.getElementById('password').value;
         var confirmPassword = document.getElementById('confirmPassword').value;
         var errorElement = document.getElementById('confirmPasswordError');
+        var confirmPasswordInput = document.getElementById('confirmPassword');
+
         if (confirmPassword !== password) {
             errorElement.textContent = 'Passwords do not match';
-            document.getElementById('confirmPassword').classList.add('error');
+            confirmPasswordInput.classList.add('error');
         } else {
             errorElement.textContent = '';
-            document.getElementById('confirmPassword').classList.remove('error');
-            document.getElementById('confirmPassword').classList.add('success');
+            confirmPasswordInput.classList.remove('error');
+            confirmPasswordInput.classList.add('success');
         }
     }
 });
