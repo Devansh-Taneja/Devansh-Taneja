af4a30bd757d4e2d6e8b544c2bf9ad2968c14c25
Devansh-Taneja <145650332+Devansh-Taneja@users.noreply.github.com>
2023-11-03 09:56:00 +0400

    cities

--- WEEK4/main.js
+++ WEEK4/main.js
@@ -1,28 +1,41 @@
-var myCity = {
-    "name": "Dubai",
-    "country": "UAE",
-    "place": "Dubai Mall"
-};
+//var myCity = {
+//"name": "Dubai",
+//"country": "UAE",
+//"place": "Dubai Mall"
+//};
 
-console.log(myCity.name); // Outputs: Dubai
-console.log(myCity.country); // Outputs: UAE
+//console.log(myCity.name); // Outputs: Dubai
+//console.log(myCity.country); // Outputs: UAE
 
-var myCity = ["Dubai", "UAE", "Dubai Mall"];
+//var myCity = ["Dubai", "UAE", "Dubai Mall"];
 
-console.log(myCity[0]); // Outputs: Dubai
+//console.log(myCity[0]); // Outputs: Dubai
 
-var myCities = [
-    {
-        "name": "Dubai",
-        "country": "UAE",
-        "place": "Dubai Mall"
-    },
-    {
-        "name": "Milan",
-        "country": "Italy",
-        "place": "Duomo Di Milano"
-    }
-];
+//var myCities = [
+// {
+//   "name": "Dubai",
+//   "country": "UAE",
+//   "place": "Dubai Mall"
+// },
+// {
+//    "name": "Milan",
+//   "country": "Italy",
+//    "place": "Duomo Di Milano"
+// }
+//];
 
 
-console.log(myCities[1].country); // Outputs: Italy
+//console.log(myCities[1].country); // Outputs: Italy
+
+var cityContainer = document.getElementById("city-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
+    ourRequest.onload = function () {
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+        btn.classList.add("hide-me");
+    };
+    ourRequest.send();
+});
