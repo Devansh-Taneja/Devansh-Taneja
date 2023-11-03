ac814b7cd9ac20129015e22cd2c50de9c0ebfa32
Devansh-Taneja <145650332+Devansh-Taneja@users.noreply.github.com>
2023-11-03 10:13:35 +0400

    Update main.js

--- WEEK4/main.js
+++ WEEK4/main.js
@@ -1,8 +1,37 @@
-
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://Devansh-Taneja.github.io/Devansh-Taneja/WEEK4/cities1.json');
-ourRequest.onload = function() {
-var ourData = JSON.parse(ourRequest.responseText);
-console.log(ourData[0]);
-};
-ourRequest.send();
+var cityContainer = document.getElementById("city-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://Devansh-Taneja.github.io/Devansh-Taneja/WEEK4/cities1.json');
+    ourRequest.onload = function () {
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+        btn.classList.add("hide-me");
+    };
+    ourRequest.send();
+});
+function renderHTML(data) {
+    var htmlString = "";
+    for (i = 0; i < data.length; i++) {
+        htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ",</br>Where you can enjoy indoor places like: " ;
+        for (ii = 0; ii < data[i].places.indoor.length; ii++) {
+            // Loop through the indoor places of the current city.
+            if (ii == 0) {
+                htmlString += data[i].places.indoor[ii];
+            } else {
+                htmlString += ", and " + data[i].places.indoor[ii];
+            }
+        }
+        htmlString += '. & enjoy outdoor places like: ';
+        // Loop through the outdoor places of the current city.
+        for (ii = 0; ii < data[i].places.outdoor.length; ii++) {
+            if (ii == 0) {
+                htmlString += data[i].places.outdoor[ii];
+            } else {
+                htmlString += " and " + data[i].places.outdoor[ii];
+            }
+        }
+        htmlString += '.</p>';
+    }
+    cityContainer.insertAdjacentHTML('beforeend', htmlString);
+}
