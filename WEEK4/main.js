d64693f47fd8ae99bc7a2dd0d2a60e91d0f3393c
Devansh-Taneja <145650332+Devansh-Taneja@users.noreply.github.com>
2023-11-03 10:04:36 +0400

    Update main.js

--- WEEK4/main.js
+++ WEEK4/main.js
@@ -1,6 +1,4 @@
 
-console.log(myCities[1].country); // Outputs: Italy
-
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://Devansh-Taneja.github.io/Devansh-Taneja/WEEK4/cities1.json');
 ourRequest.onload = function() {
