2d2aee093677d16af2063fc6780a589d9270fe8d
Devansh-Taneja <145650332+Devansh-Taneja@users.noreply.github.com>
2023-11-03 10:00:52 +0400

    Update main.js

--- WEEK4/main.js
+++ WEEK4/main.js
@@ -27,15 +27,10 @@ var myCities = [
 
 console.log(myCities[1].country); // Outputs: Italy
 
-var cityContainer = document.getElementById("city-info");
-var btn = document.getElementById("btn");
-btn.addEventListener("click", function () {
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://Devansh-Taneja.github.io/Devansh-Taneja/WEEK4/cities1.json');
-    ourRequest.onload = function () {
-        var ourData = JSON.parse(ourRequest.responseText);
-        renderHTML(ourData);
-        btn.classList.add("hide-me");
-    };
-    ourRequest.send();
-});
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://Devansh-Taneja.github.io/Devansh-Taneja/WEEK4/cities1.json');
+ourRequest.onload = function() {
+var ourData = JSON.parse(ourRequest.responseText);
+console.log(ourData[0]);
+};
+ourRequest.send();
