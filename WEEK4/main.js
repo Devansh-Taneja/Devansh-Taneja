7d304ced47a55bc950efcf79b9678923d95b589d
Devansh-Taneja <145650332+Devansh-Taneja@users.noreply.github.com>
2023-11-03 09:37:46 +0400

    Update main.js

---
+++ WEEK4/main.js
@@ -0,0 +1,28 @@
+var myCity = {
+    "name": "Dubai",
+    "country": "UAE",
+    "place": "Dubai Mall"
+};
+
+console.log(myCity.name); // Outputs: Dubai
+console.log(myCity.country); // Outputs: UAE
+
+var myCity = ["Dubai", "UAE", "Dubai Mall"];
+
+console.log(myCity[0]); // Outputs: Dubai
+
+var myCities = [
+    {
+        "name": "Dubai",
+        "country": "UAE",
+        "place": "Dubai Mall"
+    },
+    {
+        "name": "Milan",
+        "country": "Italy",
+        "place": "Duomo Di Milano"
+    }
+];
+
+
+console.log(myCities[1].country); // Outputs: Italy
