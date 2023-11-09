08fab0adff4a761183b19e0d77b93dff62922989
Devansh-Taneja <145650332+Devansh-Taneja@users.noreply.github.com>
2023-11-09 14:33:50 +0400

    MAKE CHANGES FOR THE UI

--- WEEK4/app.js
+++ WEEK4/app.js
@@ -1,56 +1,49 @@
-//  Define the API key
-const apikey = 'fdb37a9daf4252b00b4ef65eccc0c54c';
-
-//  Get references to HTML elements
-const cityinput = document.getElementById("cityInput"); // Input field for city name
+const apiKey = 'fdb37a9daf4252b00b4ef65eccc0c54c'; // Define the API key
+// Getting references to HTML elements
+const cityInput = document.getElementById("cityInput"); // Input field for city name
 const btun = document.getElementById("btun"); // Button for submitting the city name
-const weatherinfo = document.getElementById("weather-info"); // Element to display weather information
+const container = document.getElementById("weather-info"); // Element to display weather information
 
-//  Add a click event listener to the button
+// Adding a click event listener to the button
 btun.addEventListener("click", function () {
-    //  Get the city name from the input field
-    const city = cityinput.value;
+    const city = cityInput.value; // Get the city name from the input field
 
     if (city.trim() === "") {
-        // Check if the input is empty and provide an alert if it is
         alert("Please enter a valid city name");
         return;
     }
 
-    //  Create the API URL for the weather data
-    const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+    // Creating the API URL for the weather data
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-    fetch(apiurl)
-        .then((response) => {
+    fetch(apiUrl)
+        .then(response => {
             if (!response.ok) {
-                // Check if the response is not OK and throw an error if city not found
                 throw new Error("City not found");
             }
             return response.json();
         })
-        .then((data) => {
-            //  Extract weather information from the API response
-            const weatherdescription = data.weather[0].description; // Weather description
-            const maintemperature = data.main.temp; // Main temperature
-            const windspeed = data.wind.speed; // Wind speed
+        .then(data => {
+            const weatherDescription = data.weather[0].description; // Weather description
+            const mainTemperature = Math.round(data.main.temp - 273.15); // Main temp converted to °C
+            const windSpeed = data.wind.speed; // Wind speed
 
-            // Create HTML to display weather information
+            // Creating HTML to display weather information
             const weatherHTML = `
-                <p>Weather: ${weatherdescription}</p>
-                <p>Main Temperature: ${maintemperature} Kelvin</p>
-                <p>Wind Speed: ${windspeed} m/s</p>
+                <p>The Weather in ${city} is ${weatherDescription}</p>
+                <p>The Temperature is ${mainTemperature}°C with a Wind Speed of ${windSpeed} m/s</p>
+                <p> _____________________________________________________________</p>
             `;
 
-            // Display the weather information in the designated element
-            weatherinfo.innerHTML = weatherHTML;
+            // Append the new weather information to the existing content
+            container.innerHTML += weatherHTML;
+
+            renderHTML(data);
         })
-        .catch((error) => {
-            //  Handle errors
+        .catch(error => {
             if (error.message === "City not found") {
-                // Check if the error message indicates a city not found
                 alert("City does not exist. Please enter a valid city name.");
             } else {
-                // Handle other errors
                 alert("An error occurred. Please try again later.");
             }
         });
